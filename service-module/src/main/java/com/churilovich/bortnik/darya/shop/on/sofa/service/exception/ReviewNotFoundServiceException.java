c50f75d99e0a16e7ffab9024c432a1775fbea0ed
Darya Bortnik <darya.bortnik.churilovich@gmail.com>
2021-05-05 22:39:19 +0300

    refactor code [2]

--- service-module/src/main/java/com/churilovich/bortnik/darya/shop/on/sofa/service/exception/ReviewNotFoundServiceException.java
+++ service-module/src/main/java/com/churilovich/bortnik/darya/shop/on/sofa/service/exception/ReviewNotFoundServiceException.java
@@ -1,6 +1,6 @@
 package com.churilovich.bortnik.darya.shop.on.sofa.service.exception;
 
-public class ReviewNotFoundServiceException extends RuntimeException{
+public class ReviewNotFoundServiceException extends RuntimeException {
     public ReviewNotFoundServiceException(String message) {
         super(message);
     }
